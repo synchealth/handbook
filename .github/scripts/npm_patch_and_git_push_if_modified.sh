42229948078b470d93e6a93f131b163b757ea1ea
Guy Barnard <guy@nodekit.io>
2020-11-09 16:40:55 -0600

    add workflow inputs

--- .github/scripts/npm_patch_and_git_push_if_modified.sh
+++ .github/scripts/npm_patch_and_git_push_if_modified.sh
@@ -14,14 +14,14 @@ echo "${modified_files}\n"
 
 # npm version patch --no-git-tag-version
 
-git config --global user.email "ci@sync.health"
-git config --global user.name "Sync Health CI"
+git config --global user.email "$USER_EMAIL"
+git config --global user.name "$USER_NAME"
 
 mkdir -p ~/.ssh/
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git add . -A
-git commit -m "publish from Sync Studio"
+git commit -m "$MESSAGE"
 echo "Pushing branch $BRANCH"
 git push origin HEAD:$BRANCH
